d680cad13a8d177efe6c21ed1169472b04e1d8d6
Maximilian Haru Raditya <m4h4r4@gmail.com>
2012-06-01 16:14:46 +0700

    Update build configuration.

--- ClickToBuild.cmd
+++ ClickToBuild.cmd
@@ -4,9 +4,9 @@ REM Copyright 2004-2010 Castle Project - http://www.castleproject.org/
 REM Licensed under the Apache License, Version 2.0 (the "License");
 REM you may not use this file except in compliance with the License.
 REM You may obtain a copy of the License at
-REM 
+REM
 REM     http://www.apache.org/licenses/LICENSE-2.0
-REM 
+REM
 REM Unless required by applicable law or agreed to in writing, software
 REM distributed under the License is distributed on an "AS IS" BASIS,
 REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,9 @@ REM ****************************************************************************
 
 ECHO This script builds the project in Release configuration
 
-CALL buildscripts\build.cmd NET40 ClickToBuild
+SET CLICKTOBUILD=1
+CALL buildscripts\build.cmd NET40
+SET CLICKTOBUILD=
 
 IF %ERRORLEVEL% NEQ 0 GOTO err
 ECHO **************************************************************
